35ca1c66a1ca85d35a6593d18ebd6e4be381886e
pradhokot <99374274+pradhokot@users.noreply.github.com>
2022-07-15 17:21:36 +0700

    updated halaman products
    1. develop modal popup add to cart
    2. develop card-produk

--- assets/js/product.js
+++ assets/js/product.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
 	$('#tombol-filter').click(function() {
 		if ($('#procol1').hasClass('show')) {
-			$('#products-filter header .hasil-filter').show();
+			$('#products-filter header .hasil-filter').slideDown();
 		} else {
-			$('#products-filter header .hasil-filter').hide();
+			$('#products-filter header .hasil-filter').slideUp();
 		}
 	});
 });
