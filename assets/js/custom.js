9eafb7452c8d54c0d4c57e21db27a31559f330fb
pradhokot <99374274+pradhokot@users.noreply.github.com>
2022-07-19 17:20:04 +0700

    migration css framework from bootstrap 4.6 to 5.2
    1. form newsletter
    2. form filter produk
    3. form on popup add to cart

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,4 +15,24 @@ $(document).ready(function () {
     // carousel banner
     $('.carousel-banner .carousel-item').height($(window).height());
 
+    // all about navbar
+    $(window).scroll(function() {
+        if ($(window).scrollTop()) {
+            $('.navbar').addClass('on-scroll');
+        } else {
+            $('.navbar').removeClass('on-scroll');            
+        }
+    });
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            $('.navbar').removeClass('scroll');
+        } else {
+            $('.navbar').addClass('scroll');
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
 });
